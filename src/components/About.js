c02bf54e90706c8e047f83fe8b1545d96097891f
jusuuu <ben.zhang2124@gmail.com>
2021-09-15 13:56:39 +1200

    Adjusted spacing in text

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ export default function About() {
         <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
-              Hi, I'm Ben.
-              <br className="hidden lg:inline-block" />I love to learn and build websites.
+              Hi, I'm Ben. 
+              <br className="hidden lg:inline-block" /> I love to learn and build websites.
             </h1>
             <p className="mb-8 leading-relaxed">
               I am a recent graduate from computer science and an 
